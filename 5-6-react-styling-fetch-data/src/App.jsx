604a1afe178280579b1db5d51aea6e4db2e76129
hassan12361 <49165616+hassan12361@users.noreply.github.com>
2026-03-04 10:15:14 +0300

    task 2.2

--- 5-6-react-styling-fetch-data/src/App.jsx
+++ 5-6-react-styling-fetch-data/src/App.jsx
@@ -229,8 +229,16 @@ export default function App() {
      Implement filtering logic inside this useEffect.
      Dependency array MUST be: [searchTerm, users]
      ========================================================= */
-  useEffect(() => {
-    // TODO 2.2: Implement filtering users here (see lab instructions)
+useEffect(() => {
+    // TODO 2.2: Implement filtering users here
+    if (searchTerm.trim() === "") {
+      setFilteredUsers(users);
+    } else {
+      const filtered = users.filter(user => 
+        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredUsers(filtered);
+    }
   }, [searchTerm, users]);
 
   // Modal handlers (already complete)
